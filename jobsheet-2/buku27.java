6f7d2c085d26b50e38645f5f6ffd47fef9d177ba
vaqanzel <valengrehasta@gmail.com>
2024-02-21 21:33:29 +0700

    jobsheet 2

---
+++ jobsheet-2/buku27.java
@@ -0,0 +1,26 @@
+
+public class buku27 {
+    //deklarasi atribut
+    String judul;
+    String pengarang;
+    int halaman;
+    int stok;
+    int harga;
+    //deklarasi method
+    void tampilInformasi() {
+         System.out.println("Judul: " + judul);
+         System.out.println("Pengarang: " + pengarang);
+         System.out.println("Julah halaman: " + halaman);
+         System.out.println("Sisa Stok: " + stok);
+         System.out.println("Harga: Rp " + harga);
+    }
+    void terjual(int jml){
+        stok -= jml;
+    }   
+    void restock(int jml){
+        stok += jml;
+    }
+    void gantiHarga(int hrg){
+        harga = hrg;
+    }
+}
