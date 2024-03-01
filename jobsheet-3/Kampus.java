8499b9aa89db49100ce9f9a96d0fa766fdba8286
vaqanzel <valengrehasta@gmail.com>
2024-03-01 11:27:43 +0700

    modifikasi

--- jobsheet-3/Kampus.java
+++ jobsheet-3/Kampus.java
@@ -2,23 +2,5 @@ public class Kampus {
     String nama;
     String nim;
     char jenisKelamin;
-    double ipk;
-
-    public static double hitungRataIpk(Kampus[] mahasiswas) {
-        double totalIpk = 0;
-        for (Kampus mahasiswa : mahasiswas) {
-            totalIpk += mahasiswa.ipk;
-        }
-        return totalIpk / mahasiswas.length;
-    }
-
-    public static Kampus cariMahasiswaTerbaik(Kampus[] mahasiswas) {
-        Kampus mahasiswaTerbaik = mahasiswas[0];
-        for (int i = 1; i < mahasiswas.length; i++) {
-            if (mahasiswas[i].ipk > mahasiswaTerbaik.ipk) {
-                mahasiswaTerbaik = mahasiswas[i];
-            }
-        }
-        return mahasiswaTerbaik;
-    }
+    double ipk;  
 }
--- jobsheet-3/KampusMain.java
+++ jobsheet-3/KampusMain.java
@@ -5,6 +5,15 @@ public class KampusMain {
         Scanner scanner = new Scanner(System.in);
         Kampus[] mhsiswa = new Kampus[3];
 
+        
+        inputDataMahasiswa(scanner, mhsiswa);
+        tampilkanDataMahasiswa(mhsiswa);
+        double rataIpk = hitungRataIpk(mhsiswa);
+        System.out.println("Rata-rata IPK: " + rataIpk);
+        tampilkanMaxIpk(mhsiswa);
+    }
+
+    public static void inputDataMahasiswa(Scanner scanner, Kampus[] mhsiswa) {
         for (int i = 0; i < mhsiswa.length; i++) {
             System.out.println("Masukkan data mahasiswa ke- " + (i + 1));
             Kampus mahasiswa = new Kampus();
@@ -19,7 +28,8 @@ public class KampusMain {
 
             mhsiswa[i] = mahasiswa;
         }
-
+    }
+    public static void tampilkanDataMahasiswa(Kampus[] mhsiswa) {
         System.out.println();
         for (int i = 0; i < mhsiswa.length; i++) {
             System.out.println("Data Mahasiswa ke- " + (i + 1));
@@ -29,5 +39,29 @@ public class KampusMain {
             System.out.println("Nilai IPK : " + mhsiswa[i].ipk);
             System.out.println();
         }
+    }
+
+    public static double hitungRataIpk(Kampus[] mhsiswa) {
+        double totalIpk = 0;
+        for (Kampus mahasiswa : mhsiswa) {
+            totalIpk += mahasiswa.ipk;
+        }
+        return totalIpk / mhsiswa.length;
+    }
+
+    public static void tampilkanMaxIpk(Kampus[] mhsiswa) {
+        double maxIpk = mhsiswa[0].ipk;
+        int indexMax = 0;
+        for (int i = 1; i < mhsiswa.length; i++) {
+            if (mhsiswa[i].ipk > maxIpk) {
+                maxIpk = mhsiswa[i].ipk;
+                indexMax = i;
+            }
+        }
+        System.out.println("Data Mahasiswa dengan IPK Terbesar:");
+        System.out.println("Nama : " + mhsiswa[indexMax].nama);
+        System.out.println("NIM : " + mhsiswa[indexMax].nim);
+        System.out.println("Jenis Kelamin : " + mhsiswa[indexMax].jenisKelamin);
+        System.out.println("Nilai IPK : " + mhsiswa[indexMax].ipk);
     }
 }
