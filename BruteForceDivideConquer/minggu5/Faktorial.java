bb3c19746a448b4310a269032374e4063d8c4802
vaqanzel <valengrehasta@gmail.com>
2024-03-17 00:33:37 +0700

    mengubah for ke rekursi

--- BruteForceDivideConquer/minggu5/Faktorial.java
+++ BruteForceDivideConquer/minggu5/Faktorial.java
@@ -3,14 +3,22 @@ package minggu5;
 public class Faktorial {
      int nilai;
 
-     int faktorialBF(int n) {
-        int fakto = 1;
-        for(int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+    //  int faktorialBF(int n) {
+    //     int fakto = 1;
+    //     for(int i = 1; i <= n; i++) {
+    //         fakto = fakto * i;
+    //     }
+    //     return fakto;
+    // }
+
+    int faktorialBF(int n) {
+        if (n==0 || n==1) {
+            return 1;
+        } else {
+            return n * faktorialBF(n - 1);
         }
-        return fakto;
     }
-    
+
       int faktorialDC(int n) {
         if(n==1) {
             return 1;
