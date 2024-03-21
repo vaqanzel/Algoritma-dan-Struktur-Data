a2d6f4cfb5e5976b55fba7a47eaf1d25efb5c364
vaqanzel <valengrehasta@gmail.com>
2024-03-21 21:56:44 +0700

    Mengubah fungsi pada InsertionSort sehingga fungsi ini dapat melaksanakan proses sorting dengan cara descending

--- bubble-selection-insertion/minggu6/DaftarMahasiswaBerprestasi.java
+++ bubble-selection-insertion/minggu6/DaftarMahasiswaBerprestasi.java
@@ -52,7 +52,8 @@ public class DaftarMahasiswaBerprestasi {
         for (int i=1; i < listMhs.length; i++){
             Mahasiswa temp = listMhs[i];
             int j = i;
-            while (j>0 && listMhs[j-1].ipk > temp.ipk){
+            // Periksa apakah ipk dari elemen sebelumnya lebih kecil dari ipk elemen saat ini
+            while (j>0 && listMhs[j-1].ipk < temp.ipk){
                 listMhs[j] = listMhs[j-1];
                 j--;
             }
