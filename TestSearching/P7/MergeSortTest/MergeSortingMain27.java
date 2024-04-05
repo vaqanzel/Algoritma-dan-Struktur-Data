4dfdba041b1301284a0e8ae3143f87725322db57
vaqanzel <valengrehasta@gmail.com>
2024-04-05 09:54:58 +0700

    Percobaan Merge Sort

---
+++ TestSearching/P7/MergeSortTest/MergeSortingMain27.java
@@ -0,0 +1,14 @@
+package TestSearching.P7.MergeSortTest;
+
+public class MergeSortingMain27 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("sorting dengan merge sort");
+        MergeSorting27 mSort = new MergeSorting27();
+        System.out.println("data awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
