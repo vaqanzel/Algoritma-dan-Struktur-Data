51f5f9ea7adad9d004d82b9ab01e265c7690ef30
vaqanzel <valengrehasta@gmail.com>
2024-04-26 01:46:40 +0700

    Percobaan 1

---
+++ Praktikum1/Queue.java
@@ -0,0 +1,86 @@
+
+public class Queue {
+    int[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public Queue(int n) {
+        max = n;
+        data = new int[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == max;
+    }
+
+    public void peek() {
+        if (!isEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front]);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            System.out.print("Queue: ");
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i] + " ");
+                i = (i + 1) % max;
+            }
+            System.out.println(data[i] + " ");
+            System.out.println("Jumlah elemen = " + size);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void clear() {
+        if (!isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void enqueue(int dt) {
+        if (isFull()) {
+            System.out.println("Queue sudah penuh");
+        } else {
+            if (isEmpty()) {
+                front = rear = 0;
+            } else {
+                rear = (rear + 1) % max;
+            }
+            data[rear] = dt;
+            size++;
+        }
+    }
+
+    public int dequeue() {
+        int dt = 0;
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            dt = data[front];
+            size--;
+            if (isEmpty()) {
+                front = rear = -1;
+            } else {
+                front = (front + 1) % max;
+            }
+        }
+        return dt;
+    }
+}
