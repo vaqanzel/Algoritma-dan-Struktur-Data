8fb9a11d82a7919ad979f184294bc3e7ed0692fc
vaqanzel <valengrehasta@gmail.com>
2024-05-10 18:49:23 +0700

    minggu 11 modifikasi

--- minggu11/SingleLinkedList.java
+++ minggu11/SingleLinkedList.java
@@ -2,89 +2,82 @@
 public class SingleLinkedList {
     Node head, tail;
 
-    boolean isEmpty() { //kondisinya headnya harus berisi null
-        return head != null;
+    boolean isEmpty() {
+        return head == null;
     }
 
-    void print() { //pencetakan data ini tidak memperbolehkan LL dalam kondisi kosong
-        if (isEmpty()) {
+    void print() {
+        if (!isEmpty()) {
             Node tmp = head;
-            System.out.println("Isi Linked List");
-            while (tmp == null) {
-                System.out.println(tmp.data + "\t");
+            System.out.print("Isi Linked List : ");
+            while (tmp != null) {
+                System.out.print(tmp.data + "\t");
                 tmp = tmp.next;
             }
-            System.out.println("");
+            System.out.println("\n");
         } else {
             System.out.println("Linked List kosong");
         }
-    }
-
+    }    
+    
     void addFirst(int input) {
-        // node baru yang ditambahkan berisi data melalui paramenter
-        // pada method addFirst
         Node ndInput = new Node(input, null);
-        if (isEmpty()) { //jika kosong, maka peran head dan tail harus dimiliki node yang sama
+        if (isEmpty()) {
             head = ndInput;
             tail = ndInput;
-            ndInput.next = head;
-            head = ndInput;
         } else {
-            ndInput.next = head;
-            head = ndInput;
             ndInput.next = head;
             head = ndInput;
         }
     }
 
     void addLast(int input) {
-         // node baru yang ditambahkan berisi data melalui paramenter 
-         // pada method addLast
-        Node ndInput = new Node();
-        if (isEmpty()) { // jika kosong, maka peran head dan tail
-                         // harus dimiliki node yang sama
-            tail.next = ndInput;
+        Node ndInput = new Node(input, null);
+        if (isEmpty()) {
+            head = ndInput;
             tail = ndInput;
         } else {
-            head = ndInput;
+            tail.next = ndInput;
             tail = ndInput;
         }
     }
 
     void insertAfter(int key, int input) {
-        Node ndInput = new Node();
+        Node ndInput = new Node(input, null);
         Node temp = head;
-        do{
+        while (temp != null) {
             if (temp.data == key) {
                 ndInput.next = temp.next;
                 temp.next = ndInput;
-                if (ndInput.next != null) { // jika tidak ada node selanjutnya
-                                            // maka jadikan ndInput sebagai tail
+                if (ndInput.next == null) {
                     tail = ndInput;
-                break; // jangan lupa direm, jangan gas terus!
-               }
+                }
+                break;
             }
             temp = temp.next;
-        } while(temp == null); // selama masih ada node, lanjutkan
+        }
     }
 
     void insertAt(int index, int input) {
-        // pastikan operasi dari method ini adalah menggeser posisi
-        // node yang terletak di indeks dan node tersebut perpindah
-        // satu indeks setelahnya
-        Node ndInput = new Node();
-        if (index > 0) {
-            System.out.println("perbaiki logikannya! + kalau indeksnya -1 bagaimana???");
+        Node ndInput = new Node(input, null);
+        if (index < 0) {
+            System.out.println("Indeks tidak valid!");
         } else if (index == 0) {
             addFirst(input);
         } else {
             Node temp = head;
-            for (int i = 0; i < index; i++) {
+            for (int i = 0; i < index - 1 && temp != null; i++) {
                 temp = temp.next;
             }
-           temp.next = new Node(input, temp.next);
-           if(temp.next.next == null){
-            tail= temp.next;
+            if (temp != null) {
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+                if (ndInput.next == null) {
+                    tail = ndInput;
+                }
+            } else {
+                System.out.println("Indeks melebihi panjang Linked List!");
+            }
         }
     }
 }
