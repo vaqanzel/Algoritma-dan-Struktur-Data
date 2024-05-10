e9d718c31ac20dbf3c7ddf4d75108621f8d21265
vaqanzel <valengrehasta@gmail.com>
2024-05-10 19:51:27 +0700

    modifikasi percobaan 2

--- minggu11/SingleLinkedList.java
+++ minggu11/SingleLinkedList.java
@@ -80,4 +80,101 @@ public class SingleLinkedList {
             }
         }
     }
+
+    int getData(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Indeks tidak valid!");
+        }
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            if (tmp == null) {
+                throw new IndexOutOfBoundsException("Indeks melebihi panjang linked list!");
+            }
+            tmp = tmp.next;
+        }
+        if (tmp == null) {
+            throw new IndexOutOfBoundsException("Indeks melebihi panjang linked list!");
+        }
+        return tmp.data;
+    }
+
+    int indexOf(int key) {
+        Node tmp = head;
+        int index = 0;
+        while (tmp != null && tmp.data != key) {
+            tmp = tmp.next;
+            index++;
+        }
+        if (tmp != null) {
+            return index;
+        } else {
+            return -1;
+        }
+    }
+
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head.data == key) {
+            removeFirst();
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                if (temp.next.data == key) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    return;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    void removeAt(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Indeks tidak valid!");
+        }
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            if (temp == null || temp.next == null) {
+                throw new IndexOutOfBoundsException("Indeks melebihi panjang linked list!");
+            }
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        if (temp.next == null) {
+            tail = temp;
+        }
+    }
 }
