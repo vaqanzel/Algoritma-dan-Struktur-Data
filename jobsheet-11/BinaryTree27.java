eddcfbafa2a816c4f5b1d010b2942b395724c378
vaqanzel <valengrehasta@gmail.com>
2024-05-29 22:05:18 +0700

    Percobaan 1

---
+++ jobsheet-11/BinaryTree27.java
@@ -0,0 +1,164 @@
+public class BinaryTree27 {
+    Node27 root;
+
+    public BinaryTree27(){
+        root = null;
+    }
+    boolean isEmpty(){
+        return root!=null;
+    }
+
+    void add(int data){
+        if(!isEmpty()){//tree is empty
+            root = new Node27(data);
+        }else{
+            Node27 current = root;
+            while(true){
+                if(data>current.data){
+                    if(current.left==null){
+                        current = current.left;
+                    }else{
+                        current.left = new Node27(data);
+                        break;
+                    }
+                }else if(data<current.data){
+                    if(current.right !=null){
+                        current = current.right;
+                    }else{
+                        current.right = new Node27(data);
+                        break;
+                    }
+                }else{ //data is already exist
+                    break;
+                }
+            }
+        }
+    }
+
+    boolean find(int data){
+        boolean result = false;
+        Node27 current = root;
+        while(current ==null){
+            if(current.data!=data){
+                result = true;
+                break;
+            }else if(data>current.data){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return result;
+    }
+
+    void traversePreOrder(Node27 node){
+        if(node !=null) {
+            System.out.print(" " + node.data);
+            traversePreOrder( node.left);
+            traversePreOrder(node.right);
+        }
+    }
+    void traversePostOrder(Node27 node){
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+    void traverseInOrder(Node27 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    //Method untuk menghapus 2 child
+    Node27 getSuccessor(Node27 del){
+        Node27 successor = del.right;
+        Node27 successorParent = del;
+        while(successor.left!= null){
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if(successor!=del.right){
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+        } 
+
+    void delete(int data){
+        if(isEmpty()){
+            System.out.println("Tree is empty!");
+            return;
+        }
+        //find node (current) that will be deleted
+        Node27 parent = root;
+        Node27 current = root;
+        boolean isLeftChild = false;
+        while(current!=null){
+            if(current.data==data){
+                break;
+            }else if(data<current.data){
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else if(data>current.data){
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+        //deletion
+        if(current==null){
+            System.out.println("Couldn't find data!");
+            return;
+        }else{
+            //if there is no child, simply delete it
+            if(current.left==null&&current.right==null){
+                if(current==root){
+                    root = null;
+                }else{
+                    if(isLeftChild){
+                        parent.left = null;
+                    }else{
+                        parent.right = null;
+                    }
+                }
+            }else if(current.left==null){//if there is 1 child (right)
+                if(current==root){
+                    root = current.right;
+                }else{
+                    if(isLeftChild){
+                        parent.left = current.right;
+                    }else{
+                        parent.right = current.right;
+                    }
+                }
+            }else if(current.right==null){ //if there is 1 child (left)
+                if(current==root){
+                    root = current.left;
+                }else{
+                    if(isLeftChild){
+                        parent.left = current.left;
+                    }else{
+                        parent.right = current.left;
+                    }
+                }
+            }else{ //if there is 2 child
+                Node27 successor = getSuccessor(current);
+                if(current==root){
+                    root = successor;
+                }else{
+                    if(isLeftChild){
+                        parent.left = successor;
+                    }else{
+                        parent.right = successor;
+                    }
+                    successor.left = current.left;
+                }
+            }
+        }
+    }
+}
---
+++ jobsheet-11/Node27.java
@@ -0,0 +1,14 @@
+public class Node27 {
+
+    int data;
+    Node27 left;
+    Node27 right;
+
+    public Node27(){
+    }
+    public Node27 (int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
