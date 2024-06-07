2fb104f594e3eb6aa3986638552b29f56d52f6b7
vaqanzel <valengrehasta@gmail.com>
2024-06-07 08:22:28 +0700

    Percobaan 2

--- jobsheet-12/GraphMain27.java
+++ jobsheet-12/GraphMain27.java
@@ -13,25 +13,37 @@ public class GraphMain27 {
         gedung.degree(0);
         gedung.printGraph();
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Masukkan gedung asal: ");
-            int asal = scanner.nextInt();
-            System.out.print("Masukkan gedung tujuan: ");
-            int tujuan = scanner.nextInt();
+        // Scanner scanner = new Scanner(System.in);
+        // while (true) {
+        //     System.out.print("Masukkan gedung asal: ");
+        //     int asal = scanner.nextInt();
+        //     System.out.print("Masukkan gedung tujuan: ");
+        //     int tujuan = scanner.nextInt();
 
-            boolean adaJalur = gedung.cekJalur(asal, tujuan);
-            if (adaJalur) {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan gedung " + (char) ('A' + tujuan) + " bertetangga");
-            } else {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan gedung " + (char) ('A' + tujuan) + " tidak bertetangga");
-            }
+        //     boolean adaJalur = gedung.cekJalur(asal, tujuan);
+        //     if (adaJalur) {
+        //         System.out.println("Gedung " + (char) ('A' + asal) + " dan gedung " + (char) ('A' + tujuan) + " bertetangga");
+        //     } else {
+        //         System.out.println("Gedung " + (char) ('A' + asal) + " dan gedung " + (char) ('A' + tujuan) + " tidak bertetangga");
+        //     }
 
-            System.out.print("Ingin memasukkan data lagi? (ya/tidak): ");
-            String lanjut = scanner.next();
-            if (!lanjut.equalsIgnoreCase("ya")) {
-                break;
-            }
-        }
+        //     System.out.print("Ingin memasukkan data lagi? (ya/tidak): ");
+        //     String lanjut = scanner.next();
+        //     if (!lanjut.equalsIgnoreCase("ya")) {
+        //         break;
+        //     }
+        // }
+
+        GraphMatriks27 gdg = new GraphMatriks27(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
     }
 }
